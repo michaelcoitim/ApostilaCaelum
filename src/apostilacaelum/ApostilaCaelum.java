cf8e81bfd629d2fb24d3933153465046cba052b7
michaelcoitim <michaelcoitim1@gmail.com>
2022-07-25 18:07:51 -0300

    testando operador % modulo

--- src/apostilacaelum/ApostilaCaelum.java
+++ src/apostilacaelum/ApostilaCaelum.java
@@ -15,7 +15,9 @@ public class ApostilaCaelum {
     public static void main(String[] args) {
         // declarando criando variável e atribundo o valor 15
         int idade =15;
-        
+        int quatro = 2 + 2;
+        int tres = 5 - 2;
+        int um = 5 % 2;
         
         
 
@@ -23,7 +25,7 @@ public class ApostilaCaelum {
         //System.out.println("Olá mundo \nPrimeiro programa");
         //System.out.println("Primeiro programa");
         // exibindo o valor da variável idade
-        System.out.println(idade);
+        System.out.println(um);
         
     }
     
