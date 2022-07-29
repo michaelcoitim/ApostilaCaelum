c4919cfd67b0f1fc9755514c95fdf21d11fcd52d
michaelcoitim <michaelcoitim1@gmail.com>
2022-07-29 09:19:45 -0300

    Exercicios imprimir numeros de 150 a 300

---
+++ src/apostilacaelum/Exercicios.java
@@ -0,0 +1,20 @@
+/*
+ * Exercicios da postila
+ */
+package apostilacaelum;
+
+/**
+ *
+ * @author Michael Coitim
+ */
+public class Exercicios {
+    
+    public void ImprimeNumeros(int a , int b){
+        
+        for(int i=a; i > b;i++){
+            System.out.println(i);
+        }
+        
+    }
+    
+}
