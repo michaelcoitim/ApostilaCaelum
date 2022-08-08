81affa8da833c47ec61f0cb7b0dda61c607376cc
michaelcoitim <michaelcoitim1@gmail.com>
2022-08-08 15:51:55 -0300

    Tipos e classes exemplo banco (cap 4)

---
+++ src/apostilacaelum/banco/Conta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apostilacaelum.banco;
+
+/**
+ *
+ * @author Michael Coitim 
+ */
+public class Conta {
+    int numero;
+    String titular;
+    double saldo;
+    
+}
---
+++ src/apostilacaelum/banco/Programa.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+package apostilacaelum.banco;
+
+/**
+ *
+ * @author colaborador
+ */
+public class Programa {
+    public static void main(String[]args){
+        
+        new Conta();
+    }
+    
+}
